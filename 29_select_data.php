34165fb3fd4164ef0d8a9e9e2f8753b627404a60
Md Taofick <107477685+taofick221@users.noreply.github.com>
2025-03-15 13:22:12 +0600

    Update 29_select_data.php

--- 29_select_data.php
+++ 29_select_data.php
@@ -7,7 +7,7 @@
  // Create connection
  $conn = mysqli_connect($servername, $username, $password, $database);
 
- // Check connection
+ 
  if (!$conn) {
      die("Connection failed: " . mysqli_connect_error());
  }
